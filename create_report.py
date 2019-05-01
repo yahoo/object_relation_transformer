e2b80d993faf516be7e4fe4f2daf4fb9b0ecaffc
Joao Soares <jvbsoares@oath.com>
2019-05-01 23:21:17 +0000

    Write a table with main measures and draw some histograms with distributions; added an option to add current time to output folder name

--- create_report.py
+++ create_report.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python2
 # Usage example:
 # ./create_report.py --help
-# ./create_report.py --pickle eval_results/fc_transformer_bu_adaptive_test_coco_eval.pkl --out_dir report_dir_test
+# ./create_report.py --pickle eval_results/fc_transformer_bu_adaptive_test_coco_eval.pkl --out_dir reports/test_dir
 #
 # After creating the report by using the commands above, you can set up the
 # server to serve it using something like the following commands.
-# cd report_dir_test
+# cd reports/
 # python -m SimpleHTTPServer 8888
 import argparse
 from six.moves import cPickle as pickle
 from misc.report import create_report
+from datetime import datetime
 
 
 class Args:
-    OUT_DIR = 'out_dir'
+    BASE_OUT_DIR = 'out_dir'
     COCO_EVAL_PICKLE = 'pickle'
+    ADD_TIME = 'add_time'
+    NO_ADD_TIME = 'no_add_time'
 
 
 def main():
     args = _get_command_line_arguments()
-    out_dir = args[Args.OUT_DIR]
+    base_out_dir = args[Args.BASE_OUT_DIR]
+    add_time = args[Args.ADD_TIME]
+    out_dir = _get_out_dir(base_out_dir, add_time)
     pickle_path = args[Args.COCO_EVAL_PICKLE]
     coco_eval = _read_pickle(pickle_path)
     create_report(coco_eval, out_dir)
@@ -31,9 +36,25 @@ def _get_command_line_arguments():
         '--' + Args.COCO_EVAL_PICKLE,
         help='Pickle file with the COCOEvalCap object', required=True)
     parser.add_argument(
-        '--' + Args.OUT_DIR, help='Output directory', required=True)
+        '--' + Args.BASE_OUT_DIR, help='Output directory', required=True)
+    parser.add_argument(
+        '--' + Args.ADD_TIME, help='Add a timestamp to the output directory',
+        default=False, required=False, action='store_true', dest=Args.ADD_TIME)
+    parser.add_argument(
+        '--' + Args.NO_ADD_TIME,
+        help='Don\'t add a timestamp to the output directory',
+        required=False, action='store_false', dest=Args.ADD_TIME)
     args_dict = vars(parser.parse_args())
     return args_dict
+
+
+def _get_out_dir(base_out_dir, add_time):
+    if add_time:
+        date_string = datetime.now().strftime('%Y-%m-%d--%H_%M_%S')
+        out_dir = "%s_%s" % (base_out_dir, date_string)
+    else:
+        out_dir = base_out_dir
+    return out_dir
 
 
 def _read_pickle(pickle_path):
--- misc/report.py
+++ misc/report.py
@@ -1,12 +1,67 @@
 import sys
 import os
+import matplotlib.pyplot as plt
+# By using Agg, pyplot will not try to open a display
+plt.switch_backend('Agg')
+from matplotlib.figure import Figure
+from pandas import DataFrame, Series
 from pandas.io.json import json_normalize
+from typing import IO
 sys.path.append("coco-caption")
 from pycocoevalcap.eval import COCOEvalCap
 
 INDEX_HTML = 'index.html'
-IMAGE_ID_COLUMN_NAME = 'image_id'
-INDEX_COLUMN_NAME = IMAGE_ID_COLUMN_NAME
+HTML_IMAGE_WIDTH_PIXELS = 400
+SUMMARY_VALUES_COLUMN_NAME = 'Value'
+PLOT_DIR_NAME = 'plots'
+
+
+class EvalColumns:
+    IMAGE_ID = 'image_id'
+    BLEU1 = 'Bleu_1'
+    BLEU2 = 'Bleu_2'
+    BLEU3 = 'Bleu_3'
+    BLEU4 = 'Bleu_4'
+    CIDER = 'CIDEr'
+    METEOR = 'METEOR'
+    ROUGE_L = 'ROUGE_L'
+    SPICE = 'SPICE.All.f'
+    SPICE_OBJECT = 'SPICE.Object.f'
+    SPICE_RELATION = 'SPICE.Relation.f'
+    SPICE_ATTRIBUTE = 'SPICE.Attribute.f'
+    SPICE_COLOR = 'SPICE.Color.f'
+    SPICE_CARDINALITY = 'SPICE.Cardinality.f'
+    SPICE_SIZE = 'SPICE.Size.f'
+
+
+INDEX_COLUMN_NAME = EvalColumns.IMAGE_ID
+COLUMNS_FOR_HISTOGRAM_NON_SPICE = [
+    EvalColumns.BLEU1, EvalColumns.BLEU2, EvalColumns.BLEU3, EvalColumns.BLEU4,
+    EvalColumns.CIDER, EvalColumns.METEOR, EvalColumns.ROUGE_L]
+COLUMNS_FOR_HISTOGRAM_SPICE = [
+    EvalColumns.SPICE, EvalColumns.SPICE_OBJECT, EvalColumns.SPICE_RELATION,
+    EvalColumns.SPICE_ATTRIBUTE, EvalColumns.SPICE_COLOR,
+    EvalColumns.SPICE_CARDINALITY, EvalColumns.SPICE_SIZE]
+
+
+class PathForHTML:
+    """Keeps track of a regular path, as well as a relative path, which can be
+    used to create links in HTML code."""
+
+    def __init__(self, regular, relative):
+        # type: (str, str) -> None
+        self.regular = regular
+        self.relative = relative
+
+    @staticmethod
+    def from_base_and_relative(base, relative):
+        # type: (str, str) -> PathForHTML
+        return PathForHTML(os.path.join(base, relative), relative)
+
+    def join(self, to_append):
+        # type: (str) -> PathForHTML
+        return PathForHTML(os.path.join(self.regular, to_append),
+                           os.path.join(self.relative, to_append))
 
 
 def create_report(coco_eval, out_dir):
@@ -25,9 +80,76 @@ def create_report(coco_eval, out_dir):
     # For now, just fail if the directory already exists, so we don't
     # overwrite anything accidentally.
     os.makedirs(out_dir)
-    data_frame = json_normalize(coco_eval.imgToEval.values())
-    data_frame.set_index(INDEX_COLUMN_NAME, inplace=True)
     report_index_path = os.path.join(out_dir, INDEX_HTML)
     with open(report_index_path, 'w') as report_index_file:
-        report_index_file.write('This is a table with the data that was read')
-        report_index_file.write(data_frame.to_html())
+        _add_summary_table(report_index_file, coco_eval)
+        data_frame = json_normalize(coco_eval.imgToEval.values())
+        data_frame.set_index(INDEX_COLUMN_NAME, inplace=True)
+        plot_dir_for_html = PathForHTML.from_base_and_relative(
+            out_dir, PLOT_DIR_NAME)
+        os.makedirs(plot_dir_for_html.regular)
+        _add_histograms(report_index_file, plot_dir_for_html, data_frame)
+        _add_unlabeled_images(report_index_file, out_dir)
+
+
+def _add_summary_table(html_file, coco_eval):
+    # type: (IO, COCOEvalCap) -> None
+    """Write overall measures, from COCOEvalCap.eval."""
+    _write_header(html_file, 'Measures over all images')
+    summary_data_frame = Series(coco_eval.eval).to_frame(
+        SUMMARY_VALUES_COLUMN_NAME)
+    html_file.write(summary_data_frame.to_html() + '\n')
+
+
+def _add_histograms(html_file, plot_dir_for_html, data_frame):
+    # type: (IO, PathForHTML, DataFrame) -> None
+    _write_header(html_file, 'Distribution of different measures (except '
+                             'SPICE-related, which come later below)')
+    for column_name in COLUMNS_FOR_HISTOGRAM_NON_SPICE:
+        _add_histogram(html_file, data_frame, column_name,
+                       plot_dir_for_html)
+    _write_header(html_file, 'Distribution of SPICE-related measures')
+    for column_name in COLUMNS_FOR_HISTOGRAM_SPICE:
+        _add_histogram(html_file, data_frame, column_name,
+                       plot_dir_for_html)
+
+
+def _add_histogram(html_file, data_frame, column_name, plot_dir_for_html):
+    # type: (IO, DataFrame, str, PathForHTML) -> None
+    figure = _plot_histogram(data_frame, column_name)
+    image_file_name = column_name + '.jpg'
+    image_path_for_html = plot_dir_for_html.join(image_file_name)
+    _save_figure(html_file, image_path_for_html, figure)
+
+
+def _plot_histogram(data_frame, column_name):
+    # type: (DataFrame, str) -> Figure
+    figure = plt.figure()
+    # Draw the histogram into the current active axes.
+    data_frame.hist(column=column_name, ax=plt.gca(), edgecolor='black',
+                    linewidth=1.2, grid=False)
+    count = data_frame[column_name].count()
+    plt.title('%s (image count = %d)' % (column_name, count))
+    plt.ylabel('Frequency')
+    plt.xlabel(column_name)
+    return figure
+
+
+def _save_figure(html_file, image_path_for_html, figure):
+    # type: (IO, PathForHTML, Figure) -> None
+    figure.savefig(image_path_for_html.regular)
+    html_file.write("<img src='%s' width='%d'>\n" % (
+        image_path_for_html.relative, HTML_IMAGE_WIDTH_PIXELS))
+
+
+def _add_unlabeled_images(html_file, out_dir):
+    # type: (IO, str) -> None
+    _write_header(html_file, 'Unlabeled images')
+    html_file.write('to be added at some point?')
+    # TODO: implement this to allow us to view unlabeled images (no ground
+    # truth).
+
+
+def _write_header(html_file, header):
+    # type: (IO, str) -> None
+    html_file.write('<h1>%s</h1>\n' % header)
